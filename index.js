ccdfb5d46f0db20127635670a0066f1ce1351dca
rk-tanvir790 <ktanvir790@gmail.com>
2025-08-30 01:15:56 +0600

    setup Api for delete tips

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ async function run() {
             const result = await tipsCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
+        app.delete('/tips/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const result = await tipsCollection.deleteOne(filter)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
